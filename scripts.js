3484798c24032b91088e22c342699db96e95c10b
TheHumanOreo <player343@hotmail.com>
2018-09-10 02:49:59 +0100

    Several changes in this version: - compacted the site into a single page - added sections for previous pages - added placeholder text and titles - added a semi-functional contact form - navbar now links to relevant section - slightly changed css and js files so they upload correctly
    Potential issues:
    - contact form has no assigned function
    - probably others
    
    Planned additions:
    - Persistent side navbar
    - More distinction between sections
    - Functioning contact form
    - Project pages
    - Potential use of files for easier content manipulation
    - Some funky javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function changeNav() {
+function changeNav()
+{
     var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
+
+    if (x.className === "topnav")
+    {
         x.className += " responsive";
-    } else {
+    }
+    else
+    {
         x.className = "topnav";
     }
 }
